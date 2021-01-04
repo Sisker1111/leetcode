7913e7fb118a558a544bf31f81588e52d38524d4
harry1120 <harry8fan@gmail.com>
2021-01-04 23:26:51 +0800

    2021-01-04

---
+++ exercise/104. Maximum Depth of Binary Tree.c
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int traversal(struct TreeNode* node){
+    if(node == NULL)
+        return 0;
+    int left_level = traversal(node->left);
+    int right_level = traversal(node->right);
+    if(left_level >= right_level)
+        return left_level+1;
+    else
+        return right_level+1;
+    
+}
+int maxDepth(struct TreeNode* root){
+    
+    if(root == NULL)
+        return 0;
+    return traversal(root);
+    
+    
+}
